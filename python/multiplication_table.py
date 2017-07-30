e9b2c8c2df5328f3707dd1beb1d43e4830868fd4
renegadevi <philip.andersen@codeofmagi.net>
2017-07-30 11:02:34 +0200

    replaced tabs with spaces

--- python/multiplication_table.py
+++ python/multiplication_table.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 """ Multiplication table.
-	A simple practice every programmer should know how to do, basic stuff here.
+    A simple practice every programmer should know how to do, basic stuff here.
 """
 
 for row in range(1,11):
-	for column in range(1,11):
-		print(row * column, end='\t')
-	print("\n")
+    for column in range(1,11):
+        print(row * column, end='\t')
+    print("\n")
