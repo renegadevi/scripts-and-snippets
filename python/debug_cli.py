c9190058a30ef7ec99f35b0555594f15edb3ade8
renegadevi <philip.andersen@codeofmagi.net>
2018-04-15 00:49:16 +0200

    Exactly what I was looking for
    Great for debugging as well for simple programs.

---
+++ python/debug_cli.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+""" Python Fire
+
+    Automatically generating command line interfaces (CLIs) from absolutely any
+    Python object.
+
+    https://github.com/google/python-fire
+    Apache License, Version 2.0
+"""
+
+"""
+$ ./example.py hello
+Hello world!
+
+$ ./example.py hello David
+Hello David!
+
+$ ./example.py hello --name=Google
+Hello Google!
+"""
+
+import fire
+
+class Example(object):
+  def hello(self, name='world'):
+    """Says hello to the specified name."""
+    return 'Hello {name}!'.format(name=name)
+
+def main():
+  fire.Fire(Example)
+
+if __name__ == '__main__':
+  main()
+
+
