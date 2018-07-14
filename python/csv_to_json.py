62a01bbbf654212e8518121ad631954cc9bc9146
renegadevi <philip.andersen@codeofmagi.net>
2018-07-14 16:56:38 +0200

    Updated with CLI example
    - It’s now it’s own function
    - Working CLI script

--- python/csv_to_json.py
+++ python/csv_to_json.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import csv, json
-json.dumps(list(csv.reader(open('filename.csv'))))
+import sys
+import csv
+import json
+
+def csv_to_json(filename:str) -> str:
+    """ Convert CSV file to JSON data """
+    if filename.endswith('csv'):
+        return json.dumps(list(csv.reader(open(filename))))
+    raise ValueError('No CSV file specified')
+
+
+
+def main():
+    """ Example as a terminal script;
+
+    - Print it in terminal:
+    'python3 csv_to_python.py companies.csv'
+
+    - Save it to a json file
+    'python3 csv_to_python.py companies.csv >> companies.json'
+    """
+    try:
+        print(csv_to_json(sys.argv[1]))
+    except ValueError as e:
+        exit(e)
+
+if __name__ == "__main__":
+    main()
