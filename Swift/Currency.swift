07f695f9aa0a1a1fa8e4dacf3a722016e89a151d
Philip Andersen <philip.andersen@codeofmagi.net>
2019-02-11 07:05:44 +0100

    Added currency snippet

---
+++ Swift/Currency.swift
@@ -0,0 +1,13 @@
+// Solution by Leo Dabus https://stackoverflow.com/a/47172167/10572532
+
+// Get currency based on country
+extension Locale {
+    static let currency = Locale.isoRegionCodes.reduce(into: [:]) {
+        let locale = Locale(identifier: Locale.identifier(fromComponents: [NSLocale.Key.countryCode.rawValue: $1]))
+        $0[$1] = (locale.currencyCode, locale.currencySymbol)
+    }
+}
+
+Locale.currency["US"]   // (code "USD", symbol "US$")
+Locale.currency["BR"]   // (code "BRL", symbol "R$")
+Locale.currency["GB"]   // (code "GBP", symbol "£")
