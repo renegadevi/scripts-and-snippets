6b9634da9273181d433b303776b71cb591b0b39e
Philip Andersen <philip.andersen@codeofmagi.net>
2019-08-29 00:10:57 +0200

    Added UserDefaults.swift extension

---
+++ Swift/Extensions/UserDefaults.swift
@@ -0,0 +1,18 @@
+//
+//  UserDefaults.swift
+//
+//  Prints a list of UserDefaults sorted by key to the log output
+//
+
+import Foundation
+
+extension UserDefaults {
+
+    public static func getUserDefaults() {
+        let sortedKeys = UserDefaults.standard.dictionaryRepresentation().sorted{ $0.key < $1.key }
+        for (key, value) in sortedKeys {
+            NSLog("\(key) = \(value)")
+        }
+    }
+
+}
